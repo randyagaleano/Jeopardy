4ac383598a18e8887e4b8991cafb4847aa520131
randyagaleano <randyagaleano@gmail.com>
2017-03-08 14:02:45 -0500

    Ability to add score in real time

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 
 //  TOP ROW OF QUESTIONS
 var questions = [
-	{question: "QUESTION1", answer: "ANSWER", points: 100},
+	{question: "Is this the first question?", answer: 'a', points: 100},
 	{question: "QUESTION2", answer: "ANSWER", points: 100},
 	{question: "QUESTION3", answer: "ANSWER", points: 100},
 	{question: "QUESTION4", answer: "ANSWER", points: 100},
@@ -32,11 +32,11 @@ var questions = [
 	{question: "QUESTION5", answer: "ANSWER", points: 500},
 ];
 
-
+var score = 0;
 for ( i = 0; i<questions.length; i++) {
 	var tile = document.createElement('tiles');
 	tile.className = 'tiles';
-	// creates; takes styles of said className
+	// ^^^creates; takes styles of said className
 	tile.innerHTML = questions[i].points;
 	tile.setAttribute("question", questions[i].question);
 	tile.setAttribute("answer", questions[i].answer);
@@ -45,13 +45,23 @@ for ( i = 0; i<questions.length; i++) {
 
 	// function
 	tile.addEventListener ('click', function() {
+
 	var input = prompt(this.getAttribute("question"));
-
-})
-
-
-	document.body.appendChild(tile);
+	var answer = this.getAttribute("answer");
+	var points = this.getAttribute("points");
+	
+	if (input === answer) {
+		score += parseInt(points);
+		alert("Correct! You now have " + score + " points!")
+	}
+	else {
+		score -= parseInt(points);
+		alert("Ouch! You have dropped to " + score + " points, bummer.");
+	}
+});
+document.body.appendChild(tile);
 };
+
 
 
 
