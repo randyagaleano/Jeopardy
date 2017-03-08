8e604113192c3e0e23ed9b5a9057dffb35fa7f2a
randyagaleano <randyagaleano@gmail.com>
2017-03-08 12:47:01 -0500

    Working alert message.

--- main.js
+++ main.js
@@ -1,46 +1,109 @@
 
 //  TOP ROW OF QUESTIONS
 var questions = [
-	{question: "QUESTION1", answer: "ANSWER", value: 100},
-	{question: "QUESTION2", answer: "ANSWER", value: 100},
-	{question: "QUESTION3", answer: "ANSWER", value: 100},
-	{question: "QUESTION4", answer: "ANSWER", value: 100},
-	{question: "QUESTION5", answer: "ANSWER", value: 100},
+	{question: "QUESTION1", answer: "ANSWER", points: 100},
+	{question: "QUESTION2", answer: "ANSWER", points: 100},
+	{question: "QUESTION3", answer: "ANSWER", points: 100},
+	{question: "QUESTION4", answer: "ANSWER", points: 100},
+	{question: "QUESTION5", answer: "ANSWER", points: 100},
 
-	{question: "QUESTION1", answer: "ANSWER", value: 200},
-	{question: "QUESTION2", answer: "ANSWER", value: 200},
-	{question: "QUESTION3", answer: "ANSWER", value: 200},
-	{question: "QUESTION4", answer: "ANSWER", value: 200},
-	{question: "QUESTION5", answer: "ANSWER", value: 200},
+	{question: "QUESTION1", answer: "ANSWER", points: 200},
+	{question: "QUESTION2", answer: "ANSWER", points: 200},
+	{question: "QUESTION3", answer: "ANSWER", points: 200},
+	{question: "QUESTION4", answer: "ANSWER", points: 200},
+	{question: "QUESTION5", answer: "ANSWER", points: 200},
 
-	{question: "QUESTION1", answer: "ANSWER", value: 300},
-	{question: "QUESTION2", answer: "ANSWER", value: 300},
-	{question: "QUESTION3", answer: "ANSWER", value: 300},
-	{question: "QUESTION4", answer: "ANSWER", value: 300},
-	{question: "QUESTION5", answer: "ANSWER", value: 300},
+	{question: "QUESTION1", answer: "ANSWER", points: 300},
+	{question: "QUESTION2", answer: "ANSWER", points: 300},
+	{question: "QUESTION3", answer: "ANSWER", points: 300},
+	{question: "QUESTION4", answer: "ANSWER", points: 300},
+	{question: "QUESTION5", answer: "ANSWER", points: 300},
 
-	{question: "QUESTION1", answer: "ANSWER", value: 400},
-	{question: "QUESTION2", answer: "ANSWER", value: 400},
-	{question: "QUESTION3", answer: "ANSWER", value: 400},
-	{question: "QUESTION4", answer: "ANSWER", value: 400},
-	{question: "QUESTION5", answer: "ANSWER", value: 400},
+	{question: "QUESTION1", answer: "ANSWER", points: 400},
+	{question: "QUESTION2", answer: "ANSWER", points: 400},
+	{question: "QUESTION3", answer: "ANSWER", points: 400},
+	{question: "QUESTION4", answer: "ANSWER", points: 400},
+	{question: "QUESTION5", answer: "ANSWER", points: 400},
 
-	{question: "QUESTION1", answer: "ANSWER", value: 500},
-	{question: "QUESTION2", answer: "ANSWER", value: 500},
-	{question: "QUESTION3", answer: "ANSWER", value: 500},
-	{question: "QUESTION4", answer: "ANSWER", value: 500},
-	{question: "QUESTION5", answer: "ANSWER", value: 500},
-]
+	{question: "QUESTION1", answer: "ANSWER", points: 500},
+	{question: "QUESTION2", answer: "ANSWER", points: 500},
+	{question: "QUESTION3", answer: "ANSWER", points: 500},
+	{question: "QUESTION4", answer: "ANSWER", points: 500},
+	{question: "QUESTION5", answer: "ANSWER", points: 500},
+];
 
 
 for ( i = 0; i<questions.length; i++) {
-	var tiles = document.createElement('tiles');
-	tiles.className = 'tiles';
+	var tile = document.createElement('tiles');
+	tile.className = 'tiles';
 	// creates; takes styles of said className
+	tile.innerHTML = questions[i].points;
+	tile.setAttribute("question", questions[i].question);
+	tile.setAttribute("answer", questions[i].answer);
+	tile.setAttribute("points", questions[i].points);
 
-	tiles.innerHTML = questions[i].value;
-	tiles.setAttribute("question", questions[i].question);
+
+	// function
+	tile.addEventListener ('click', function() {
+	var input = prompt(this.getAttribute("question"));
+
+})
 
 
-	document.body.appendChild(tiles);
-}
+	document.body.appendChild(tile);
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
